9dd09057764c5f027bef3967ea657cdb975883e9
TanerDeniz <85178314+TanerDeniz@users.noreply.github.com>
2026-03-20 14:19:21 +0300

    Update quality_checks_silver.sql

--- tests/quality_checks_silver.sql
+++ tests/quality_checks_silver.sql
@@ -56,4 +56,57 @@ select distinct prd_line from silver.crm_prd_info;
 -- Expectation: No Results
 select * from silver.crm_prd_info where prd_end_dt < prd_start_dt;
 
+--========================================
+--Checking 'silver.crm_sales_details'
+--========================================
+--Check For Invalid Dates
+--Expectation : No Invalid Dates
+select nullif(sls_due_dt,0) as sls_due_dt from bronze.crm_sales_details
+where sls_due_dt<=0 or len(sls_due_dt)!=8 or sls_due_dt>20500101 or sls_due_dt<19000101
 
+--Check for Invalid Date Orders (Order Date>Shipping/Due Dates)
+--Expectation : No Results
+
+select * from silver.crm_sales_details
+where sls_order_dt>sls_ship_dt or sls_order_dt>sls_due_dt
+
+--Check Data Consistency
+--Expectation : No Results
+
+select distinct sls_sales,sls_quantity,sls_price from silver.crm_sales_details
+where sls_sales!=sls_quantity*sls_price or sls_sales is null
+or sls_quantity is null or sls_price is null or sls_sales<=0 or sls_quantity<=0
+or sls_price<=0 order by sls_sales,sls_quantity,sls_price
+
+--========================================
+--Checking 'silver.erp_cust_az12'
+--========================================
+--Identify Out-od-Range Dates
+--Expectation : Birtdates between 1924-01-01 and Today
+select distinct bdate from silver.erp_cust_az12
+where bdate>'1924-01-01' or bdate<getdate()
+
+--Data Standardization and Consistency
+
+select distinct gen from silver.erp_cust_az12;
+
+--========================================
+--Checking 'silver.erp_loc_a101'
+--========================================
+--Data Standardization & Consistency
+
+select distinct cntry from silver.erp_loc_a101
+order by cntry
+
+--========================================
+--Checking 'silver.erp_px_cat_g1v1'
+--========================================
+--Check For Unwanted Spaces
+--Expectation : No Results
+
+select * from silver.erp_px_cat_g1v1
+where cat != trim(cat) or subcat != trim(subcat) or maintance != trim(maintance);
+
+--Data Standardization & Consistency
+
+select distinct maintance from silver.erp_px_cat_g1v1
